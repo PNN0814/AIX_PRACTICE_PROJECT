c56dd71ec9b669324f4f58d845d1fe9f90dc1d7c
폰냥냥 <wjdrlghd09@naver.com>
2025-10-01 18:22:14 +0900

    Update __init__.py
    __init__ 파일 최신화

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,7 +1,4 @@
 from .database import engine, session_factory
-from .modules import Base, UserInfo
-
+from .modules import Base, UserInfo, AdminInfo
 from .schemas import UserBase, UserCreate, UserRead, UserUpdate, UserDelete, UserPwUpdate, UserLogin, BoardCreate, BoardUpdate, BoardRead, BoardList, CommentCreate, CommentRead
-# schmas추가된 것(신효빈) : BoardCreate, BoardUpdate, BoardRead, BoardList, CommentCreate, CommentRead
 from .admin_schemas import AdminLoginRequest, AdminLoginResponse, AdminPasswordChange, PostBase, PostCreate, PostUpdate
-
