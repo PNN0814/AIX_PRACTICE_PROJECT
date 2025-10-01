fda21f6da60db1b2735e5d214cdab577829a912b
shb216246 <shb216246@gmail.com>
2025-10-01 17:24:27 +0900

    Update __init__.py
    스키마 값 추가

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,5 +1,7 @@
 from .database import engine, session_factory
 from .modules import Base, UserInfo
 
-from .schemas import UserBase, UserCreate, UserRead, UserUpdate, UserDelete, UserPwUpdate, UserLogin
+from .schemas import UserBase, UserCreate, UserRead, UserUpdate, UserDelete, UserPwUpdate, UserLogin, BoardCreate, BoardUpdate, BoardRead, BoardList, CommentCreate, CommentRead
+# schmas추가된 것(신효빈) : BoardCreate, BoardUpdate, BoardRead, BoardList, CommentCreate, CommentRead
 from .admin_schemas import AdminLoginRequest, AdminLoginResponse, AdminPasswordChange, PostBase, PostCreate, PostUpdate
+
